f40e7ed078ce5463f226a80acd47029fb4c6c1a3
uncolorr <uncolored.eyes@gmail.com>
2018-09-10 21:27:28 +0300

    Изменил способ отображения скачивания(убрал уведомления)

--- app/src/main/java/com/example/uncolor/vkmusic/services/download/DownloadService.java
+++ app/src/main/java/com/example/uncolor/vkmusic/services/download/DownloadService.java
@@ -23,6 +23,7 @@ import com.example.uncolor.vkmusic.Apis.ApiResponse;
 import com.example.uncolor.vkmusic.R;
 import com.example.uncolor.vkmusic.application.App;
 import com.example.uncolor.vkmusic.models.BaseMusic;
+import com.example.uncolor.vkmusic.services.MusicService;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -41,13 +42,15 @@ import okhttp3.ResponseBody;
 
 public class DownloadService extends IntentService implements ApiResponse.ApiFailureListener {
 
+    public static final String ACTION_DOWNLOAD_STARTED = "com.example.uncolor.action.DOWNLOAD_STARTED";
+    public static final String ACTION_DOWNLOAD_COMPLETED = "com.example.uncolor.action.DOWNLOAD_COMPLETED";
+    public static final String ACTION_DOWNLOAD_FAILURE = "com.example.uncolor.action.DOWNLOAD_FAILURE";
+
+    public static final String ARG_MUSIC = "music";
+
     public DownloadService() {
         super("Download Service");
     }
-
-    private NotificationCompat.Builder notificationBuilder;
-    private NotificationManager notificationManager;
-    private int totalFileSize;
 
     private BaseMusic music;
 
@@ -67,22 +70,7 @@ public class DownloadService extends IntentService implements ApiResponse.ApiFai
     @Override
     protected void onHandleIntent(Intent intent) {
         App.Log("onHandleIntent service");
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationBuilder = new NotificationCompat.Builder(this, App.NOTIFICATION_CHANNEL_ID)
-                .setContentTitle("Загрузка")
-                .setContentText("Загрузка файла")
-                .setAutoCancel(false)
-                .setChannelId(App.NOTIFICATION_CHANNEL_ID)
-                .setLargeIcon(BitmapFactory
-                        .decodeResource(this.getResources(), R.mipmap.ic_launcher));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            notificationBuilder.setSmallIcon(R.drawable.ic_status_bar);
-            notificationBuilder.setColor(getResources().getColor(R.color.colorMain));
-        } else {
-            notificationBuilder.setSmallIcon(R.drawable.ic_status_bar);
-        }
-        File outputFile =
-                new File(Environment
+        File outputFile = new File(Environment
                         .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
                         music.getArtist() + " - " + music.getTitle() + ".mp3");
         String uriFromFile = Uri.fromFile(outputFile).toString();
@@ -90,19 +78,14 @@ public class DownloadService extends IntentService implements ApiResponse.ApiFai
         Uri uri = FileProvider.getUriForFile(getApplicationContext(),
                 App.getProviderAuthority(),
                 outputFile);
-        Intent i = new Intent(Intent.ACTION_VIEW, uri);
-        i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        PendingIntent contentIntent =
-                PendingIntent.getActivity(this, 0,
-                        i, 0);
-        notificationBuilder.setContentIntent(contentIntent);
-        notificationManager.notify(0, notificationBuilder.build());
         initDownload();
     }
 
     private void initDownload() {
-        App.Log("init download: " + music.getDownload());
-        Api.getSource().downloadFile(music.getDownload()).enqueue(ApiResponse.getCallback(getDownloadCallback(), this));
+        onDownloadStarted(ACTION_DOWNLOAD_STARTED);
+        Api.getSource().downloadFile(music.getDownload()).enqueue(ApiResponse
+                .getCallback(getDownloadCallback(), this));
+
     }
 
     private ApiResponse.ApiResponseListener<ResponseBody> getDownloadCallback() {
@@ -135,7 +118,7 @@ public class DownloadService extends IntentService implements ApiResponse.ApiFai
         int timeCount = 1;
         while ((count = bis.read(data)) != -1) {
             total += count;
-            totalFileSize = (int) (fileSize / (Math.pow(1024, 2)));
+            int totalFileSize = (int) (fileSize / (Math.pow(1024, 2)));
             double current = Math.round(total / (Math.pow(1024, 2)));
 
             int progress = (int) ((total * 100) / fileSize);
@@ -149,53 +132,52 @@ public class DownloadService extends IntentService implements ApiResponse.ApiFai
 
                 download.setCurrentFileSize((int) current);
                 download.setProgress(progress);
-                sendNotification(download);
                 timeCount++;
             }
 
             output.write(data, 0, count);
         }
-        onDownloadComplete();
+        App.Log("output path: " + outputFile.getAbsolutePath());
+        music.setLocalPath(outputFile.getAbsolutePath());
+        onDownloadComplete(ACTION_DOWNLOAD_COMPLETED);
         output.flush();
         output.close();
         bis.close();
 
     }
 
-    private void sendNotification(Download download) {
-        App.Log("send notification");
-        sendIntent(download);
-        notificationBuilder.setProgress(100, download.getProgress(), false);
-        notificationBuilder.setContentText("Скачивание файла " + download.getCurrentFileSize() + "/" + totalFileSize + " MB");
-        notificationManager.notify(0, notificationBuilder.build());
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        App.Log("onDestroy");
     }
 
-    private void sendIntent(Download download) {
+
+    private void sendIntent(String action) {
         App.Log("send intent");
-        Intent intent = new Intent(App.APP_MESSAGE_PROGRESS);
-        intent.putExtra("download", download);
-        LocalBroadcastManager.getInstance(DownloadService.this).sendBroadcast(intent);
+        Intent intent = new Intent();
+        intent.setAction(action);
+        intent.putExtra(ARG_MUSIC, music);
+        sendBroadcast(intent);
     }
 
-    private void onDownloadComplete() {
+    private void onDownloadComplete(String action) {
         App.Log("onDownloadComplete");
-        Download download = new Download();
-        download.setProgress(100);
-        sendIntent(download);
-        notificationManager.cancel(0);
-        notificationBuilder.setProgress(0, 0, false);
-        notificationBuilder.setContentText("Файл загружен в папку загрузок");
-        int id = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
-        notificationManager.notify(id, notificationBuilder.build());
+        sendIntent(action);
+    }
+
+    private void onDownloadStarted(String action) {
+        App.Log("onDownloadStarted");
+        sendIntent(action);
     }
 
     @Override
     public void onTaskRemoved(Intent rootIntent) {
-        notificationManager.cancel(0);
+
     }
 
     @Override
     public void onFailure(int code, String message) {
-        App.Log(message);
+        sendIntent(ACTION_DOWNLOAD_FAILURE);
     }
 }
