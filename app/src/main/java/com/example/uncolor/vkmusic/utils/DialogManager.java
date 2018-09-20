901e4ab9e5d0670391fccfa37b6a0538a1189e6c
uncolorr <uncolored.eyes@gmail.com>
2018-09-20 12:42:31 +0300

    Создал класс для создания диалоговых окон

---
+++ app/src/main/java/com/example/uncolor/vkmusic/utils/DialogManager.java
@@ -0,0 +1,32 @@
+package com.example.uncolor.vkmusic.utils;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+
+/**
+ * Created by Uncolor on 16.09.2018.
+ */
+
+public class DialogManager {
+
+    public static void showDialog(Context context, String message, DialogInterface.OnClickListener listener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(message);
+        builder.setCancelable(false);
+        builder.setNegativeButton("CANCEL", getNegativeButtonListener());
+                builder.setPositiveButton("OK", listener);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private static DialogInterface.OnClickListener getNegativeButtonListener(){
+        return new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        };
+    }
+}
