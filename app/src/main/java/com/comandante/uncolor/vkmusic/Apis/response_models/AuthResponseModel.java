bfa3cac97724d54f1ebf5bdb96ce9ee0dfb21c67
uncolor <sapozhnikov.v@postuf.com>
2019-08-18 18:02:06 +0300

    Change Auth response model and create class with auth response statuses

--- app/src/main/java/com/comandante/uncolor/vkmusic/Apis/response_models/AuthResponseModel.java
+++ app/src/main/java/com/comandante/uncolor/vkmusic/Apis/response_models/AuthResponseModel.java
@@ -4,18 +4,60 @@ import androidx.annotation.Nullable;
 
 import com.google.gson.annotations.SerializedName;
 
-/**
- * Created by Uncolor on 04.09.2018.
- */
-
 public class AuthResponseModel {
 
+    @Nullable
     @SerializedName("status")
     private String status;
 
     @Nullable
     @SerializedName("token")
     private String token;
+
+    @Nullable
+    @SerializedName("error")
+    private String error;
+
+    @Nullable
+    @SerializedName("error_type")
+    private String errorType;
+
+    @Nullable
+    @SerializedName("error_description")
+    private String errorDescription;
+
+    @Nullable
+    @SerializedName("captcha_sid")
+    private String captchaSid;
+
+    @Nullable
+    @SerializedName("captcha_img")
+    private String captchaImg;
+
+    @Nullable
+    public String getError() {
+        return error;
+    }
+
+    @Nullable
+    public String getErrorType() {
+        return errorType;
+    }
+
+    @Nullable
+    public String getErrorDescription() {
+        return errorDescription;
+    }
+
+    @Nullable
+    public String getCaptchaSid() {
+        return captchaSid;
+    }
+
+    @Nullable
+    public String getCaptchaImg() {
+        return captchaImg;
+    }
 
     public String getStatus() {
         return status;
@@ -26,3 +68,4 @@ public class AuthResponseModel {
         return token;
     }
 }
+
---
+++ app/src/main/java/com/comandante/uncolor/vkmusic/Apis/response_models/ResponseStatus.java
@@ -0,0 +1,6 @@
+package com.comandante.uncolor.vkmusic.Apis.response_models;
+
+public class ResponseStatus {
+    public static final String ERROR_INVALID_CLIENT = "invalid_client";
+    public static final String ERROR_NEED_CAPTCHA = "need_captcha";
+}
