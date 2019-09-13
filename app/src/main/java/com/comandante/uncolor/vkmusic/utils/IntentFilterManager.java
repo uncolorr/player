ec79007915035ad9b1c2113195032f00eadf05f5
uncolor <sapozhnikov.v@postuf.com>
2019-09-13 12:09:12 +0300

    Delete unnecessary intent filter

--- app/src/main/java/com/comandante/uncolor/vkmusic/utils/IntentFilterManager.java
+++ app/src/main/java/com/comandante/uncolor/vkmusic/utils/IntentFilterManager.java
@@ -1,37 +1,25 @@
 package com.comandante.uncolor.vkmusic.utils;
 
-import android.app.DownloadManager;
 import android.content.IntentFilter;
 
-import com.comandante.uncolor.vkmusic.main_activity.settings_fragment.SettingsFragment;
 import com.comandante.uncolor.vkmusic.services.download.DownloadService;
-import com.comandante.uncolor.vkmusic.services.download.NewDownloadService;
-import com.comandante.uncolor.vkmusic.services.music.NewMusicService;
+import com.comandante.uncolor.vkmusic.services.music.MusicService;
 
 /**
  * Created by Uncolor on 10.09.2018.
  */
 
 public class IntentFilterManager {
-    public static IntentFilter getMusicIntentFilter() {
+    public static IntentFilter getTempMusicIntentFilter() {
         IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(NewMusicService.ACTION_PLAY);
-        intentFilter.addAction(NewMusicService.ACTION_NEXT);
-        intentFilter.addAction(NewMusicService.ACTION_PREVIOUS);
-        intentFilter.addAction(NewMusicService.ACTION_PAUSE_OR_RESUME);
-        //intentFilter.addAction(MusicService.ACTION_PLAYER_RESUME);
-        intentFilter.addAction(NewMusicService.ACTION_BEGIN_PLAYING);
-        intentFilter.addAction(NewMusicService.ACTION_CLOSE);
-        intentFilter.addAction(DownloadService.ACTION_DOWNLOAD_FAILURE);
-        intentFilter.addAction(DownloadService.ACTION_DOWNLOAD_COMPLETED);
+        intentFilter.addAction(MusicService.ACTION_PLAY);
+        intentFilter.addAction(MusicService.ACTION_PAUSE);
+        intentFilter.addAction(MusicService.ACTION_RESUME);
+        intentFilter.addAction(MusicService.ACTION_CLOSE);
+        intentFilter.addAction(MusicService.ACTION_BEGIN_PLAYING);
         intentFilter.addAction(DownloadService.ACTION_DOWNLOAD_STARTED);
-        intentFilter.addAction(SettingsFragment.ACTION_CLEAR_CACHE);
-        return intentFilter;
-    }
-
-    public static IntentFilter getDownloadIntentFilter(){
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        intentFilter.addAction(DownloadService.ACTION_DOWNLOAD_COMPLETED);
+        intentFilter.addAction(DownloadService.ACTION_DOWNLOAD_FAILURE);
         return intentFilter;
     }
 }
